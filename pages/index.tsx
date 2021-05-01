f3ef0f4ba008f31fac3581a605899636c5e9ad1f
Hekmat Hamidi <theonehamidi@gmail.com>
2021-05-01 06:05:20 -0400

    changed bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 // import Head from 'next/head';
-import Player from '../components/Player';
+import Player from "../components/Player";
 
-import { Playlist } from '../interfaces/playlist.interface';
+import { Playlist } from "../interfaces/playlist.interface";
 
 const playlist: Playlist = {
   title: "Tom Clancy's Without Remorse",
@@ -9,26 +9,25 @@ const playlist: Playlist = {
     "John Clark, a Navy SEAL, goes on a path to avenge his wife's murder only to find himself inside of a larger conspiracy.",
   sources: [
     {
-      file: 'https://fast.noss.workers.dev/17QYnb2rDQOpg57zHPOeyMyDRVcQ-jH-R',
-      type: 'video/mp4',
-      label: '1080p',
+      file: "https://fast.noss.workers.dev/17QYnb2rDQOpg57zHPOeyMyDRVcQ-jH-R",
+      type: "video/mp4",
+      label: "1080p",
       default: true,
     },
     {
-      file: 'https://fast.noss.workers.dev/17QYnb2rDQOpg57zHPOeyMyDRVcQ-jH-R',
-      type: 'video/mp4',
-      label: '720p',
+      file: "https://fast.noss.workers.dev/17QYnb2rDQOpg57zHPOeyMyDRVcQ-jH-R",
+      type: "video/mp4",
+      label: "720p",
     },
   ],
 
-  image:
-    'https://m.media-amazon.com/images/M/MV5BZDkxYzYwMjAtYjM0MS00YThlLWFmNzctM2E0ZGJkMThkYjU1XkEyXkFqcGdeQXZ3ZXNsZXk@._V1_.jpg',
+  image: "https://image.tmdb.org/t/p/original/fPGeS6jgdLovQAKunNHX8l0avCy.jpg",
   tracks: [
     {
       file:
-        'https://thirsty-keller-bed1cc.netlify.app/api/subtitle?imdb=tt0499097',
-      label: 'English',
-      kind: 'subtitles',
+        "https://thirsty-keller-bed1cc.netlify.app/api/subtitle?imdb=tt0499097",
+      label: "English",
+      kind: "subtitles",
     },
   ],
 };
