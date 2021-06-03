ca20f47d8b6c2fe3b65381005945c2efdac73c92
Hekmat Hamidi <theonehamidi@gmail.com>
2021-06-03 18:05:37 -0400

    manual token

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,9 @@ async function customHeaderRequestFetch(e) {
     n = new Request(t, {
       headers: {
         range: e.request.headers.get('range'),
-        Authorization: 'Bearer ' + a,
+        Authorization:
+          'Bearer ' +
+          'ya29.a0AfH6SMB90DYJ1oNNMtRtrAe1kX1BP-q14RECI9tDJpWGavkc9gHwlilFrWEwc-xPc1K-4ljiYL4TCpZPxM6LuzuLXK7rrEnO4SxXyEEOCN_AIIXbXaUPIV9x-6INkMVxmUxbh8ZLwNven3wWW_sn09vklwtnhtU',
       },
     });
   return fetch(n);
